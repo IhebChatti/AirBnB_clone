827193d69e0a45b0290e4346df7ef562badc3862
KoeusIss <sebri.issam@gmail.com>
2020-06-30 17:24:33 +0100

    commit the testt for user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -10,4 +10,20 @@ class TestUser(unittest.TestCase):
     """
         TestUser class
     """
-    pass
+
+    def test_should_create_user_instance(self):
+        """
+        Test
+        """
+        user = User()
+        self.assertIsInstance(user, User)
+
+    def test_should_creat_user_variables(self):
+        """
+        Test
+        """
+        user = User()
+        self.assertIsInstance(user.email, str)
+        self.assertIsInstance(user.password, str)
+        self.assertIsInstance(user.first_name, str)
+        self.assertIsInstance(user.last_name, str)
