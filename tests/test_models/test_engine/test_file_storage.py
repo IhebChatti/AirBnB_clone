12a07b13c797680713cb1bfa3115fd8269dca956
IhebChatti <iheb.chatti@holbertonschool.com>
2020-07-01 10:39:29 +0100

    Add tests for file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,11 +2,26 @@
 """
     TestFileStorage module
 """
-import untittest
+import unittest
+from models import storage
+from models.engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
     """
         TestFileStorage class
     """
-    pass
+
+    def test_instance_storage_form_FileStorage(self):
+        """
+            Test
+        """
+        self.assertIsInstance(storage, FileStorage)
+
+    def test_objects_should_be_dictionary(self):
+        """
+            Test
+        """
+        s = storage.all()
+        self.assertIsInstance(s, dict)
