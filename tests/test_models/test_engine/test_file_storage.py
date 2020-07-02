f066c0c4285bd198a34949282c22e9862811fd11
KoeusIss <sebri.issam@gmail.com>
2020-07-02 12:52:13 +0100

    committ the test for task five

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,7 +5,7 @@
 import unittest
 from models import storage
 from models.engine.file_storage import FileStorage
-from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
@@ -25,3 +25,34 @@ class TestFileStorage(unittest.TestCase):
         """
         s = storage.all()
         self.assertIsInstance(s, dict)
+
+    def test_file_path_instance(self):
+        """
+        Test raise error
+        """
+        with self.assertRaises(AttributeError):
+            f = FileStorage.__file_path
+
+    def test_objects_instance(self):
+        """
+        Test raise error
+        """
+        with self.assertRaises(AttributeError):
+            f = FileStorage.__objects
+
+    def test_all_method(self):
+        """
+        Test all method
+        """
+        d = storage.all()
+        self.assertIsInstance(d, dict)
+
+    def test_new_method(self):
+        """
+        Test new method
+        """
+        l = storage.all()
+        obj = BaseModel()
+        storage.new(obj)
+        ll = storage.all()
+        self.assertEqual(len(l), len(ll))
